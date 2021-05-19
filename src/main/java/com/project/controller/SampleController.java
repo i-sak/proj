8786a48b5446bb92d241c4c5bbd0d4ce166ca9fb
kimisak <isak7263@gmail.com>
2021-05-19 22:09:08 +0900

    Controller initBinder 메서드 예제 추가

--- src/main/java/com/project/controller/SampleController.java
+++ src/main/java/com/project/controller/SampleController.java
@@ -1,14 +1,19 @@
 package com.project.controller;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.project.domain.SampleDTO;
 import com.project.domain.SampleDTOList;
+import com.project.domain.TodoDTO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -17,6 +22,13 @@ import lombok.extern.log4j.Log4j;
 @RequestMapping("/sample/*")
 @Log4j
 public class SampleController {
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		binder.registerCustomEditor(java.util.Date.class, new CustomDateEditor(dateFormat, false));
+		log.info("initBinder call");
+	}
 	
 	@RequestMapping("")
 	public void basic() {
@@ -46,6 +58,12 @@ public class SampleController {
 	public String ex02Bean(SampleDTOList list) {
 		log.info("list dtos : " + list);
 		return "ex02Bean";
+	}
+	
+	@GetMapping("/ex03")
+	public String ex03(TodoDTO todo) {
+		log.info("todo: " + todo );
+		return "ex03";
 	}
 	
 }
